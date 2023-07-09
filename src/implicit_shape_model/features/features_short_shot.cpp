e4ed1e6422e9a62dddb0d8eebe5f40f52a9ca3f5
Viktor Seib <vseib@uni-koblenz.de>
2023-07-09 08:47:46 +0200

    up log radius

--- src/implicit_shape_model/features/features_short_shot.cpp
+++ src/implicit_shape_model/features/features_short_shot.cpp
@@ -90,8 +90,9 @@ namespace ism3d
         else
             m_min_radius = 0.0;
 
+        // TODO VS log radius does not work -->
         double ln_rmin = m_min_radius == 0 ? 0 : log(m_min_radius);
-        double ln_rmax_rmin = m_min_radius == 0 ? 0 : log(m_radius/m_min_radius);
+        double ln_rmax_rmin = m_min_radius == 0 ? 0 : log(m_radius/m_min_radius); // --> maybe m_min_radius must be relative here?
 
         #pragma omp parallel for num_threads(m_numThreads)
         for(int i = 0; i < keypoints->points.size(); i++)
@@ -153,8 +154,11 @@ namespace ism3d
         float raw_r;
         if(m_log_radius)
         {
+            std::cout << "debug: " << m_r_bins << " " << log(r) << " " << ln_rmin << " " << ln_rmax_rmin << std::endl;
             raw_r = (m_r_bins - 1) * (log(r) - ln_rmin) / ln_rmax_rmin + 1;
+            std::cout << "debug: " << raw_r << std::endl;
             bin_r = int(raw_r);
+            std::cout << "debug: " << bin_r << std::endl;
         }
         else
         {
